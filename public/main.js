aa0485db18c22ee2f7ba2bd4c9034edc10a30356
Tobi Akanji <tobia807@gmail.com>
2019-10-10 08:24:34 +0100

    Moved main.html into public. \nLaid down concept plans for the project in the index.html, main.html, main.js, and style.css (all in the public folder).

--- public/main.js
+++ public/main.js
@@ -1,3 +1,29 @@
 $(document).ready(()=>{
+    //sign-up user
+        //check for input errors after a blur/keyup on the given input
+        //for errors, add custom error-defined attributes to the given input, else remove
+        //use css display property to manipulate the input error correction fields
+        //post input details to users db resource if no input errors
+        //hide sign-up section and display sign-in section
 
+    //sign-in user
+        //check for input errors after a blur/keyup on the given input
+        //verify user input
+        //empty signed-in db resource
+        //post input details to `signed-in` db resource if no input errors
+        var signin_username;
+
+    //generate scratchcard
+        //post generated scratchcard to `generated_scratchcards` db resource
+        //this resource will have a main property named after the signed-in username
+        //the other properties fall under this usernamed property
+        //add the generated scratchcard to the `generated` column of the scratchcard table
+
+    //buy scratchcard
+        //a scratchcard is not bought until paid for
+        //delete scratchcards from generated_scratchcards and update the `generated` column after buying
+        //post bought scratchcards in `bought_scratchcards` db rsource
+        //this resource will have a main property named after the signed-in username
+        //the other properties fall under this usernamed property
+        //add the bought scratchcard to the `bought` column of the scratchcard table
 });
